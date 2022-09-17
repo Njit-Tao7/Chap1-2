2ea712cb838a70babf2a2799a9f224e0fa87ad7b
Njit-Tao7 <tao7@njit.edu>
2022-09-17 19:43:59 -0400

    app build

---
+++ redwoodblog/web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,19 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+import ArticlesCell from 'src/components/ArticlesCell'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+      <ArticlesCell />
+
+      <main>Home</main>
+
+
+
+    </>
+  )
+}
+
+export default HomePage
